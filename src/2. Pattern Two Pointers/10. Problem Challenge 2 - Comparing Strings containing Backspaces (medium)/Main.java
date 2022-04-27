e0d12735b5b4a6a0ac9442eddd7c983a6a80898f
Ahmed Akram <eng.akramovic@gmail.com>
2022-04-27 17:24:23 +0200

    Comparing Strings containing Backspaces (medium)

---
+++ src/2. Pattern Two Pointers/10. Problem Challenge 2 - Comparing Strings containing Backspaces (medium)/Main.java
@@ -0,0 +1,35 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static boolean backspaceCompare(String s, String t) {
+        int p1 = s.length() - 1, p2 = t.length() - 1;
+        while (p1 >= 0 || p2 >= 0) {
+            p1 = updateIndex(s,p1);
+            p2 = updateIndex(t,p2);
+            if (p1 == 0 && p2 == 0) return true;
+            if (p1 == 0 || p2 == 0) return false;
+            if (s.charAt(p1) != t.charAt(p2)) return false;
+            p1--;p2--;
+        }
+        return true;
+    }
+    public static int updateIndex(String str,int index){
+        int counter = 0;
+        while (index >= 0){
+            if (str.charAt(index) == '#')   counter++;
+            else if (counter > 0) counter--;
+            else break;
+            index--;
+        }
+        return index;
+    }
+
+    public static void main(String[] args) {
+	    boolean result = backspaceCompare("ab#c","ad#c");
+        System.out.println(result);
+    }
+}
