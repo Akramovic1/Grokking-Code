07e6368389b5b58c4b21dbba2779e90265e8c02c
Ahmed Akram <eng.akramovic@gmail.com>
2022-05-07 18:37:59 +0200

    Kth Largest Number (easy)

---
+++ src/13. Pattern Top _K_ Elements/3. Kth Largest Number (easy)/Main.java
@@ -0,0 +1,21 @@
+import com.sun.source.tree.Tree;
+
+import java.util.*;
+
+public class Main {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        for(int i = 0; i < k; i++)
+            minHeap.add(nums[i]);
+        for(int i = k; i < nums.length; i++)
+            if(nums[i] > minHeap.peek()){
+                minHeap.poll();
+                minHeap.add(nums[i]);
+            }
+        return minHeap.peek();
+    }
+
+    public static void main(String[]args){
+
+    }
+}
